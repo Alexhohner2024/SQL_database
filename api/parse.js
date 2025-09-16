b06bd377bc4b65ec57f0cd31c42d3d387f208a66
Alexhohner2024 <alex.babaian2008@gmail.com>
2025-09-16 18:05:12 +0300

    Update parse.js

--- api/parse.js
+++ api/parse.js
@@ -1,5 +1,5 @@
 import puppeteer from 'puppeteer-core';
-import chromium from 'chrome-aws-lambda';
+import chromium from '@sparticuz/chromium';
 
 export default async function handler(req, res) {
   if (req.method !== 'GET') {

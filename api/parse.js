7153c0dd6e78dece25dc1f19ec1c102069e8a3e0
Alexhohner2024 <alex.babaian2008@gmail.com>
2025-09-16 16:59:33 +0300

    Update parse.js

--- api/parse.js
+++ api/parse.js
@@ -1,1 +1,14 @@
-
+export default async function handler(req, res) {
+  if (req.method !== 'GET') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+  
+  const { plate } = req.query;
+  
+  if (!plate) {
+    return res.status(400).json({ error: 'Plate number required' });
+  }
+  
+  // TODO: парсинг сайта
+  res.json({ plate, company: 'TODO' });
+}
